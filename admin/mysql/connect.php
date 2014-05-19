71883b823d215daa591e430ff61a3618722c186e
OrangeeWeb <agne240@me.com>
2014-05-19 10:15:45 +0200

    connect file

---
+++ admin/mysql/connect.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+//
+$db_host = "localhost";
+$db_username = "root";
+$db_password = "root";
+$db_name = "orangee_test";
+
+
+$db = new PDO('mysql:host='.$db_host.';dbname='.$db_name,$db_username,$db_password);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+
+//$main = "http://scrolldier.com/";
+$main = "http://localhost/"; ?>
