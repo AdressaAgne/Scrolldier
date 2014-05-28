e1c319b9f1e0f9071c1b68a17b67a7de7441000a
OrangeeWeb <agne240@me.com>
2014-05-28 13:42:45 +0200

    well fuck

--- new_deck_choose.php
+++ new_deck_choose.php
@@ -19,6 +19,7 @@ if (!isset($_SESSION['username'])) {
 	<title>Scrolls - New Deck</title>
 	<link rel="stylesheet" href="<?php echo($main) ?>css/style.css" />
 	<link rel="icon" type="image/png" href="img/bunny.png">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
 	<!--[if lt IE 9]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
@@ -30,9 +31,14 @@ if (!isset($_SESSION['username'])) {
 	 
  	<div class="container">
  		<div class="div-3 div-center">
- 			<h3 class="align-center">Import from:</h3>
- 			<a href="<?php echo($main) ?>new/import/scrollguide" class="div-half btn-modern align-center btn-choose">Scrollsguide<br /><small>SG Deckbuilder</small></a>
- 			<a href="<?php echo($main) ?>new/import/in-game" class="div-half btn-modern align-center btn-choose">In-Game<br /><small>Available soon</small></a>
+ 			<h3 class="align-center">Import from: <i class="icon-import"></i></h3>
+ 			<a href="<?php echo($main) ?>new/import/scrollguide" class="div-half btn-modern align-center btn-choose"> 
+ 				<i class="icon-sg"></i>
+ 				</a>
+ 			<a href="<?php echo($main) ?>new/import/in-game/" class="div-half btn-modern align-center btn-choose">
+ 				<i class="icon-scrollsLogo"></i>
+ 			</a>
+ 			<a href="<?php echo($main) ?>deckbuilder/" class="btn-modern div-full-choose align-center btn-choose">Build New Deck<br /><small>Scrolldier deckbuilder</small></a>
  		</div>
  	</div>
  <?php include("inc_/footer.php"); ?>
